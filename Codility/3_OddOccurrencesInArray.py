5bf02cbd5104e7cac573e2e5da2175826ee378c8
changjinhan <cjhanist@gmail.com>
2025-03-06 21:57:10 +0900

    Odd Occurrences In Array 2

--- Codility/3_OddOccurrencesInArray.py
+++ Codility/3_OddOccurrencesInArray.py
@@ -5,9 +5,15 @@ Task: Odd Occurrences In Array
 
 from collections import Counter
 
-# Time complexity: O(N)
-def solution(A):
+def solution1(A):
     count_dic = dict(Counter(A))
     for key, val in count_dic.items():
         if val % 2 == 1:
             return key
+
+# Time complexity: O(N)
+def solution2(A):
+    result = 0
+    for number in A:
+        result ^= number # XOR operation 1)commutative: a⊕b=b⊕a, 2)associative (a⊕b)⊕c=a⊕(b⊕c)
+    return result
