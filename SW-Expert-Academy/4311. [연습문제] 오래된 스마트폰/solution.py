0cdff2ef442d20c62232f7aa2c5ecb6a2876cce3
한창진 <changjinhan@hanchangjin-ui-MacBookPro.local>
2025-04-17 23:28:09 +0900

    SWEA 4311 solved

--- SW-Expert-Academy/4311. [연습문제] 오래된 스마트폰/solution.py
+++ SW-Expert-Academy/4311. [연습문제] 오래된 스마트폰/solution.py
@@ -1,44 +1,13 @@
-# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
-# 아래 표준 입출력 예제 필요시 참고하세요.
-
-# 표준 입력 예제
-'''
-a = int(input())                        정수형 변수 1개 입력 받는 예제
-b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제 
-d = float(input())                      실수형 변수 1개 입력 받는 예제
-e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
-h = input()                             문자열 변수 1개 입력 받는 예제
-'''
-
-# 표준 출력 예제
-'''
-a, b = 6, 3
-c, d, e = 1.0, 2.5, 3.4
-f = "ABC"
-print(a)                                정수형 변수 1개 출력하는 예제
-print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
-print(c, d, e)                          실수형 변수 3개 출력하는 예제
-print(f)                                문자열 1개 출력하는 예제
-'''
+# 4311. [연습문제] 오래된 스마트폰
+# 메모리 62,464 kb / 실행시간 361 ms
 
 import sys
+# from collections import deque
 
-
-'''
-      아래의 구문은 input.txt 를 read only 형식으로 연 후,
-      앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-      여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-      아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-
-      따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
-      아래 구문을 사용하기 위해서는 import sys가 필요합니다.
-
-      단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
-'''
 sys.stdin = open("./sample_input.txt", "r")
 
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+
 op_dict = {
     1: '+',
     2: '-',
@@ -46,65 +15,63 @@ op_dict = {
     4: '/',
 }
 
-def update_only_num(count, cur):
-    if count > M:
+def recur(cur, val):
+    if cur == 3:
         return
-    
-    count += 1
-    for i in arr:
-        val = cur * 10 + i
-        if val < len(nums) and nums[val] > count:
-            nums[val] = count
-            update_only_num(count + 1, val)
-
-
-def update_with_ops(cur):
-    if nums[cur] > M or (cur > len(nums) - 1):
-       return
-    
-    for op in ops:
-        for j in arr:
-            val = cur
-            if op == '+':
-                val += j
-            elif op == '-':
-                val -= j
-            elif op == '*':
-                val *= j
-            elif op == '/':
-                if j == 0:
-                    continue
-                val //= j
-            if 0 <= val < len(nums) and (nums[val] > nums[cur] + 2): 
-                nums[val] = nums[cur] + 2
-                update_with_ops(val)
+    for d in arr:
+        num = 10 * val + d
+        if num < 1000 and visited[num] > cur + 1:
+            visited[num] = cur + 1
+            only_nums.append(num)
+            q.append(num)
+            recur(cur + 1, num)
 
 
 for test_case in range(1, T + 1):
-    # Test case 입력
     N, O, M = map(int, input().split())
-    arr = map(int, input().split())
-    arr = list(arr)
+    arr = list(map(int, input().split()))
     ops = [op_dict[i] for i in list(map(int, input().split()))]
     W = int(input())
-    result = 0
-    
-    # 연산자 없이 만들 수 있는 모든 숫자에 대해 터치 횟수 저장
-    nums = [M+1] * 1000
-    update_only_num(0, 0)
+    visited = [M+1] * 1000
 
-    if nums[W] <= M:
-        result = nums[W]
-        print(f'#{test_case} {result}')
+    # 숫자만 눌러서 만들 수 있는 값 기록
+    # q = deque()
+    q = []
+    only_nums = []
+    recur(0, 0)
+
+    # 숫자만 눌러서 만들 수 있으면 종료
+    if visited[W] <= M:
+        print(f'#{test_case} {visited[W]}')
         continue
-
-    # 연산자 사용해서 만들 수 있는 모든 숫자에 대해 터치 횟수 저장
-    for i in range(1000):
-        update_with_ops(i)
-
-    if nums[W] <= M - 1:
-        result = nums[W] + 1
+    
+    # 연산자 + 숫자 눌러서 만들 수 있는 값 기록
+    while q:
+        # cur = q.popleft()
+        cur = q.pop(0)
+        for num in only_nums:
+            click_cnt = visited[cur] + 1 + len(str(num)) # 연산자 + 숫자 누르는 횟수
+            if click_cnt + 1 > M: # = 누르는 횟수 반영
+                continue
+            for op in ops:
+                val = None
+                if op == '+':
+                    val = cur + num
+                elif op == '-':
+                    val = cur - num
+                elif op == '*':
+                    val = cur * num
+                elif op == '/':
+                    if num == 0:
+                        continue
+                    val = cur // num
+                if val is not None and 0 <= val < 1000:
+                    if visited[val] > click_cnt:
+                        visited[val] = click_cnt
+                        q.append(val)
+    
+    # 정답 출력
+    if visited[W] <= M - 1:
+        print(f'#{test_case} {visited[W] + 1}')
     else:
-        result = -1
-
-    print(f'#{test_case} {result}')
+        print(f'#{test_case} -1')
