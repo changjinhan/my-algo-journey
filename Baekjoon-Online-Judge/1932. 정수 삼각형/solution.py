c770ad83fc6e97126b7d429f7ac4d0e60ee95df9
changjinhan <cjhanist@gmail.com>
2025-05-21 16:27:41 +0900

    BOJ 1932

--- Baekjoon-Online-Judge/1932. 정수 삼각형/solution.py
+++ Baekjoon-Online-Judge/1932. 정수 삼각형/solution.py
@@ -1,5 +1,5 @@
 # 1932. 정수 삼각형
-# 메모리 43,372kb / 실행시간 124ms
+# 메모리 36,504kb / 실행시간 104ms
 
 import sys
 sys.stdin = open("./sample_input.txt", "r")
@@ -7,19 +7,16 @@ sys.stdin = open("./sample_input.txt", "r")
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
-    triangle = [[0]*N for _ in range(N)]
-    for i in range(N):
-        triangle[i][:i+1] = list(map(int, input().split()))
-    dp = [[0]*N for _ in range(N)]
-    dp[0][0] = triangle[0][0]
+    arr = [list(map(int,input().strip().split())) for _ in range(N)]
+    dp = arr
     for i in range(1, N):
         for j in range(i+1):
             if j == 0:
-                dp[i][j] = dp[i-1][j] + triangle[i][j]
+                dp[i][j] += dp[i-1][j]
             elif j == i:
-                dp[i][j] = dp[i-1][j-1] + triangle[i][j]
+                dp[i][j] += dp[i-1][j-1]
             else:
-                dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + triangle[i][j]
+                dp[i][j] += max(dp[i-1][j-1], dp[i-1][j])
     print(max(dp[N-1]))
 
 
